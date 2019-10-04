37d85c1520d1f45b4794f9141e60e45f48bec643
Rafa Narciso <narciso.rafa611@gmail.com>
2019-10-04 16:34:12 -0700

    First draft of snake game, functional but ugly

---
+++ main.py
@@ -0,0 +1,151 @@
+import pygame
+import random
+
+FRAME_RATE = 10
+FRAME_WIDTH = 500
+FRAME_HEIGHT = 500
+SNAKE_SIZE = 20
+FOOD_RAD = 4
+
+class Snake(object):
+    def __init__(self):
+        self.x = (FRAME_WIDTH - SNAKE_SIZE)/2
+        self.y = (FRAME_HEIGHT - SNAKE_SIZE)/2
+        self.length = 1
+        self.x_vel = SNAKE_SIZE
+        self.y_vel = 0
+
+        self.body = [self.head()]
+
+    def head(self):
+        return pygame.Rect(self.x, self.y, SNAKE_SIZE, SNAKE_SIZE)
+
+    def move_left(self):
+        self.x_vel = - SNAKE_SIZE
+        self.y_vel = 0
+
+    def move_right(self):
+        self.x_vel = SNAKE_SIZE
+        self.y_vel = 0
+
+    def move_up(self):
+        self.x_vel = 0
+        self.y_vel = -SNAKE_SIZE
+
+    def move_down(self):
+        self.x_vel = 0
+        self.y_vel = SNAKE_SIZE
+
+    def increase_size(self):
+        self.body.insert(0, pygame.Rect(self.x, self.y, SNAKE_SIZE, SNAKE_SIZE))
+
+    def draw(self, win):
+        self.x = (self.x + self.x_vel) % FRAME_WIDTH
+        self.y = (self.y + self.y_vel) % FRAME_HEIGHT
+        for piece in self.body:
+            if piece.colliderect(self.head()):
+                return False
+        self.body.insert(0, self.head())
+        self.body.pop()
+        drawn = []
+        for piece in self.body:
+            pygame.draw.rect(win, (0,255,0), piece)
+
+            drawn.append(piece)
+
+        return True
+
+    def __del__(self):
+        pass
+
+class Food(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.show = False
+
+    @classmethod
+    def random(cls):
+        return Food(random.randint(0, FRAME_WIDTH) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2,
+                    random.randint(0, FRAME_HEIGHT) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2)
+
+    def check_eat(self, snake):
+        return snake.head().collidepoint(self.x, self.y)
+
+    def draw(self, win):
+        pygame.draw.circle(win, (255,0,0), (self.x, self.y), FOOD_RAD)
+
+def display_text(win, text, x, y, size, color):
+    font = pygame.font.SysFont('comicsansms', size)
+    rendering = font.render(text, True, color)
+    win.blit(rendering, (x, y))
+
+def main():
+    # start the window
+    win = pygame.display.set_mode((500,500))
+
+    #initial setup
+    pygame.display.set_caption('Snakey Snake')
+    clock = pygame.time.Clock()
+
+    pygame.font.init()
+
+    # init the snake
+    snake = Snake()
+
+    # init the current food item
+    food = Food.random()
+
+    # do the intro sequence
+    pygame.draw.rect(win, (255,255,255), (150,200, 200, 100))
+    display_text(win, 'START', 150, 200, 40, (0,0,0))
+    pygame.display.update()
+    wait = True
+    while wait:
+        clock.tick(FRAME_RATE)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    wait = False
+
+    wait = True
+    while wait:
+        clock.tick(FRAME_RATE)
+        if event.type == pygame.QUIT:
+            wait = False
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    snake.move_left()
+                elif event.key == pygame.K_RIGHT:
+                    snake.move_right()
+                elif event.key == pygame.K_UP:
+                    snake.move_up()
+                elif event.key == pygame.K_DOWN:
+                    snake.move_down()
+                elif event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+                    wait = False
+
+        if food.check_eat(snake):
+            snake.increase_size()
+            del food
+            food = Food.random()
+
+        win.fill((0,0,0))
+        food.draw(win)
+        wait = snake.draw(win)
+        pygame.display.update()
+
+    # do clean up stuff here
+    pygame.draw.rect(win, (255,0,0), (150,200, 200, 100))
+    display_text(win, 'DED', 150, 200, 40, (0,0,0))
+    pygame.display.update()
+
+    wait = True
+    while wait:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and (event.key == pygame.K_RETURN or event.key == pygame.K_ESCAPE):
+                wait = False
+    return
+
+main()
