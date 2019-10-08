effa98861e8bc24c2ffbcf9098b13c7efc4986ec
Rafa Narciso <narciso.rafa611@gmail.com>
2019-10-08 16:17:08 -0700

    Added walls and wall collision, centered text on start and end boxes

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import random
 FRAME_RATE = 10
 FRAME_WIDTH = 500
 FRAME_HEIGHT = 500
+WALL_WIDTH = 5
 SNAKE_SIZE = 20
 FOOD_RAD = 4
 
 class Snake(object):
     def __init__(self):
-        self.x = (FRAME_WIDTH - SNAKE_SIZE)/2
-        self.y = (FRAME_HEIGHT - SNAKE_SIZE)/2
+        self.x = (FRAME_WIDTH - SNAKE_SIZE)/2 + WALL_WIDTH
+        self.y = (FRAME_HEIGHT - SNAKE_SIZE)/2 + WALL_WIDTH
         self.length = 1
         self.x_vel = SNAKE_SIZE
         self.y_vel = 0
@@ -40,8 +41,12 @@ class Snake(object):
         self.body.insert(0, pygame.Rect(self.x, self.y, SNAKE_SIZE, SNAKE_SIZE))
 
     def draw(self, win):
-        self.x = (self.x + self.x_vel) % FRAME_WIDTH
-        self.y = (self.y + self.y_vel) % FRAME_HEIGHT
+        self.x = (self.x + self.x_vel)
+        if self.x < WALL_WIDTH or self.x >= FRAME_WIDTH+WALL_WIDTH:
+            return False
+        self.y = (self.y + self.y_vel)
+        if self.y < WALL_WIDTH or self.y >= FRAME_HEIGHT+WALL_WIDTH:
+            return False
         for piece in self.body:
             if piece.colliderect(self.head()):
                 return False
@@ -50,7 +55,6 @@ class Snake(object):
         drawn = []
         for piece in self.body:
             pygame.draw.rect(win, (0,255,0), piece)
-
             drawn.append(piece)
 
         return True
@@ -66,8 +70,8 @@ class Food(object):
 
     @classmethod
     def random(cls):
-        return Food(random.randint(0, FRAME_WIDTH) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2,
-                    random.randint(0, FRAME_HEIGHT) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2)
+        return Food(random.randint(0, FRAME_WIDTH) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2 + WALL_WIDTH,
+                    random.randint(0, FRAME_HEIGHT) // SNAKE_SIZE * SNAKE_SIZE + SNAKE_SIZE//2 + WALL_WIDTH)
 
     def check_eat(self, snake):
         return snake.head().collidepoint(self.x, self.y)
@@ -80,15 +84,19 @@ def display_text(win, text, x, y, size, color):
     rendering = font.render(text, True, color)
     win.blit(rendering, (x, y))
 
+def text_size(text, size):
+    font = pygame.font.SysFont('comicsansms', size)
+    return font.size(text)
+
 def main():
     # start the window
-    win = pygame.display.set_mode((500,500))
+    win = pygame.display.set_mode((FRAME_WIDTH+WALL_WIDTH*2, FRAME_HEIGHT+WALL_WIDTH*2))
 
     #initial setup
     pygame.display.set_caption('Snakey Snake')
     clock = pygame.time.Clock()
 
-    pygame.font.init()
+    init = pygame.font.init()
 
     # init the snake
     snake = Snake()
@@ -97,8 +105,14 @@ def main():
     food = Food.random()
 
     # do the intro sequence
-    pygame.draw.rect(win, (255,255,255), (150,200, 200, 100))
-    display_text(win, 'START', 150, 200, 40, (0,0,0))
+    pygame.draw.lines(win, (255,255,255), True, [(0,0),
+                                                 (0,FRAME_HEIGHT+WALL_WIDTH*2),
+                                                 (FRAME_WIDTH+WALL_WIDTH*2, FRAME_HEIGHT+WALL_WIDTH*2),
+                                                 (FRAME_WIDTH+WALL_WIDTH*2, 0)], 5)
+    pygame.draw.rect(win, (255,255,255), (FRAME_WIDTH//2 - 100 + WALL_WIDTH, FRAME_HEIGHT//2 - 50 + WALL_WIDTH,
+                                          200, 100))
+    text_width, text_height = text_size('START', 40)
+    display_text(win, 'START', (FRAME_WIDTH-text_width)//2 + WALL_WIDTH, (FRAME_HEIGHT-text_height)//2 + WALL_WIDTH, 40, (0,0,0))
     pygame.display.update()
     wait = True
     while wait:
@@ -132,13 +146,19 @@ def main():
             food = Food.random()
 
         win.fill((0,0,0))
+        pygame.draw.lines(win, (255,255,255), True, [(0,0),
+                                                 (0,FRAME_HEIGHT+WALL_WIDTH*2),
+                                                 (FRAME_WIDTH+WALL_WIDTH*2, FRAME_HEIGHT+WALL_WIDTH*2),
+                                                 (FRAME_WIDTH+WALL_WIDTH*2, 0)], 5)
         food.draw(win)
         wait = snake.draw(win)
         pygame.display.update()
 
     # do clean up stuff here
-    pygame.draw.rect(win, (255,0,0), (150,200, 200, 100))
-    display_text(win, 'DED', 150, 200, 40, (0,0,0))
+    pygame.draw.rect(win, (255,0,0), (FRAME_WIDTH//2 - 100 + WALL_WIDTH, FRAME_HEIGHT//2 - 50 + WALL_WIDTH,
+                                          200, 100))
+    text_width, text_height = text_size('DED', 40)
+    display_text(win, 'DED', (FRAME_WIDTH-text_width)//2 + WALL_WIDTH, (FRAME_HEIGHT-text_height)//2 + WALL_WIDTH, 40, (0,0,0))
     pygame.display.update()
 
     wait = True
